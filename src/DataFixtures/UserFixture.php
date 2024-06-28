3afa6f1c47e0179cf4853d3c60e740a158a32508
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2024-06-28 23:46:22 +0200

    user fixture improved

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -10,30 +10,30 @@ use Faker\Factory;
 class UserFixture extends Fixture
 {
     public const USER_ARRAY = [
-        ['name' => 'John','role' => 'Admin', 'ref' => 'John'],
-        ['name' => 'Pierre','role' => 'Admin', 'ref' => 'Pierre'],
-        ['name' => 'Marcel','role' => 'Subscriber', 'ref' => 'Marcel'],
-        ['name' => 'Hugo','role' => 'Subscriber', 'ref' => 'Hugo'],
-        ['name' => 'Nicolas','role' => 'Moderator', 'ref' => 'Nicolas' ],
-        ['name' => 'Frédéric','role' => 'Moderator', 'ref' => 'Frédéric'],
-        ['name' => 'Emmanuel','role' => 'Subscriber', 'ref' => 'Emmanuel'],
-        ['name' => 'Valentin','role' => 'Subscriber', 'ref' => 'Valentin'],
+        ['name' => 'John','role' => 'Admin', 'ref' => 'John', 'email' => 'John@example.com', 'logo' => !null],
+        ['name' => 'Pierre','role' => 'Admin', 'ref' => 'Pierre', 'email' => 'Pierre@example.com', 'logo' => !null],
+        ['name' => 'Marcel','role' => 'Subscriber', 'ref' => 'Marcel', 'email' => 'Marcel@example.com', 'logo' => null],
+        ['name' => 'Hugo','role' => 'Subscriber', 'ref' => 'Hugo', 'email' => 'Hugo@example.com', 'logo' => !null],
+        ['name' => 'Nicolas','role' => 'Moderator', 'ref' => 'Nicolas' , 'email' => 'Nicolas@example.com', 'logo' => null],
+        ['name' => 'Frédéric','role' => 'Moderator', 'ref' => 'Frédéric', 'email' => 'Frédéric@example.com', 'logo' => null],
+        ['name' => 'Emmanuel','role' => 'Subscriber', 'ref' => 'Emmanuel', 'email' => 'Emmanuel@example.com', 'logo' => null],
+        ['name' => 'Valentin','role' => 'Subscriber', 'ref' => 'Valentin', 'email' => 'Valentin@example.com', 'logo' => null],
     ];
 
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
 
-        foreach (self::USER_ARRAY as ['name' => $name,'role' => $role, 'ref' => $ref]) {
+        foreach (self::USER_ARRAY as ['name' => $name,'role' => $role, 'ref' => $ref, 'email' => $email, 'logo' => $logo]) {
             $user = new User();
 
             $user->setName($name)
                 ->setPassword($faker->password())
-                ->setEmail($faker->safeEmail())
+                ->setEmail($email)
                 ->setRole($role)
             ;
 
-            if($name === 'Hugo' || $name === 'John') {
+            if($logo) {
                 $user->setLogo('https://picsum.photos/200');
             }
 
