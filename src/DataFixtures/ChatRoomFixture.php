41131d6025de81e68514db77b6ece6d7d69ba9f3
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2024-06-29 01:30:32 +0200

    trick comment fixture

--- src/DataFixtures/ChatRoomFixture.php
+++ src/DataFixtures/ChatRoomFixture.php
@@ -14,6 +14,7 @@ class ChatRoomFixture extends Fixture implements DependentFixtureInterface
     {
         $faker = Factory::create('fr_FR');
 
+        $i = 0;
         //* create one or two comment for each user in the chatRoom
         foreach (UserFixture::USER_ARRAY as ['ref' => $ref]) {
             $chatRoom = new ChatRoom();
@@ -24,7 +25,18 @@ class ChatRoomFixture extends Fixture implements DependentFixtureInterface
                 ->setUser($this->getReference($ref))
             ;
 
+            if($i === 0) {
+                $chatRoom = new ChatRoom();
+
+                $chatRoom->setContent($faker->text(400))
+                    ->setCreatedAt(\DateTimeImmutable::createFromMutable($date->modify('+1 month')))
+                    ->setUser($this->getReference($ref))
+                ;
+            }
+
             $manager->persist($chatRoom);
+
+            $i++;
         }
 
         $manager->flush();
