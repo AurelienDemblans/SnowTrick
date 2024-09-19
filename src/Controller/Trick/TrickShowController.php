77dccb054acb9b4c721e8a79cd92c1d49d897fd0
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2024-09-19 23:33:40 +0200

    load more comment button working

--- src/Controller/Trick/TrickShowController.php
+++ src/Controller/Trick/TrickShowController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Trick;
 
 use App\Entity\Trick;
+use App\Repository\TrickCommentRepository;
 use App\Repository\TrickPictureRepository;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,21 +14,44 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class TrickShowController extends AbstractController
 {
+    public const COMMENT_OFFSET = 0;
+    public const COMMENT_LIMIT = 5;
+
     public function __construct(
         private readonly TrickRepository $trickRepository,
         private readonly TrickPictureRepository $trickPictureRepository,
-        private readonly SerializerInterface $serializer
+        private readonly SerializerInterface $serializer,
+        private readonly TrickCommentRepository $commentRepository
     ) {
 
     }
 
     #[Route(
-        '/trick/{id}/{slug}',
+        '/trick/{id<\d+>}/{slug}',
         name: 'trick',
         methods:Request::METHOD_GET
     )]
     public function showTrick(Trick $trick): Response
     {
-        return $this->render('trick.html.twig', ['trick' => $trick]);
+        $comments = $this->commentRepository->findCommentsByTrickPaginated($trick, self::COMMENT_OFFSET, self::COMMENT_LIMIT);
+
+        return $this->render('trick.html.twig', ['trick' => $trick, 'comments' => $comments]);
     }
+
+    #[Route(
+        '/trick_comments/{id<\d+>}/comments_offset/{offset<\d+>}',
+        name: 'trick_comments',
+        methods:Request::METHOD_GET
+    )]
+    public function loadMoreComments(Trick $trick, int $offset = self::COMMENT_LIMIT): Response
+    {
+        $comments = $this->commentRepository->findCommentsByTrickPaginated($trick, $offset, self::COMMENT_LIMIT);
+
+        $moreComments = $this->commentRepository->findCommentsByTrickPaginated($trick, $offset + self::COMMENT_LIMIT, 1);
+        $hasMoreComments = count($moreComments) > 0 ? true : false;
+
+        return $this->render('trickComment.html.twig', ['comments' => $comments, 'hasMoreComments' => $hasMoreComments]);
+    }
+
+
 }
