7576cf8b8274709333b16bc54f8badb78353bd43
DESKTOP-9MTB132\Aurel <demblans.dev@gmail.com>
2024-10-13 23:36:39 +0200

    DONE add trick

--- src/Controller/Trick/AddTrickController.php
+++ src/Controller/Trick/AddTrickController.php
@@ -3,11 +3,8 @@
 namespace App\Controller\Trick;
 
 use App\Entity\Trick;
-use App\Exception\FormException;
 use App\Form\TrickFormType;
-use App\Service\PictureFactory;
-use App\Service\TrickAddFactory;
-use App\Service\VideoFactory;
+use App\Service\TrickFormProcessor;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,10 +15,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class AddTrickController extends AbstractController
 {
     public function __construct(
-        private readonly TrickAddFactory $trickAddFactory,
-        private readonly PictureFactory $pictureFactory,
-        private readonly VideoFactory $videoFactory,
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly TrickFormProcessor $trickFormProcessor
     ) {
 
     }
@@ -39,16 +34,10 @@ class AddTrickController extends AbstractController
         $form = $this->createForm(TrickFormType::class, $newTrick);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $filledTrick = $form->getData();
-            $picturesList = $form->get('trickPictures')->getData();
-            $videosList = $form->get('trickVideos')->getData();
-            $videosListUrl = $form->get('trickVideosUrl')->getData();
 
             $this->entityManager->beginTransaction();
             try {
-                $trickPictures = $this->pictureFactory->createPictureFromList($picturesList);
-                $trickVideos = $this->videoFactory->createVideoFromList($videosList, $videosListUrl);
-                $trick = $this->trickAddFactory->createTrick($filledTrick, $trickPictures, $trickVideos);
+                $trick = ($this->trickFormProcessor)($form);
 
                 $this->entityManager->persist($trick);
 
@@ -56,9 +45,15 @@ class AddTrickController extends AbstractController
                 $this->entityManager->commit();
             } catch (\Throwable $th) {
                 $this->entityManager->rollback();
-                if ($th instanceof FormException) {
-                    throw new FormException($th->getMessage());
-                }
+
+                $this->addFlash(
+                    'error_form',
+                    $th->getMessage()
+                );
+
+                return $this->render('addTrick.html.twig', [
+                    'form' => $form,
+                ]);
             }
 
             $this->addFlash(
